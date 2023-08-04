bcc4f207b1bed1bf31eee8ef35142338703e4cdf
phet2309 <phet2309@gmail.com>
2023-08-04 14:50:42 -0400

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Het-Patel',
   plugins: [react(), glsl({
     include: '**/*.glsl', // Set the pattern for GLSL files
-  })],
-  base: '/Het-Patel/'
+  })]
 })
